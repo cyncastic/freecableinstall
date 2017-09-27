df4aadeaae042c1e7d26089b6fff1e547c40a635
John D. Moran <john@mtmcall.com>
2017-09-27 12:25:37 -0400

    asdfasdfasdfasdfa

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -34,3 +34,14 @@ if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
   )
   document.head.appendChild(msViewportStyle)
 }
+
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/59cbca6ec28eca75e4622ae0/default';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+})();
