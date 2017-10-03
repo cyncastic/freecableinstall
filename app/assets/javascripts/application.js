b5985810d7365c12d2af7c0678b77f7ad99739bc
John D. Moran <john@mtmcall.com>
2017-10-03 16:57:15 -0400

    asdf

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,15 +36,6 @@ if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
 
 
 // Tawk snippet for adding the option to talk with customers on site
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-  s1.async=true;
-  s1.src='https://embed.tawk.to/59cbca6ec28eca75e4622ae0/default';
-  s1.charset='UTF-8';
-  s1.setAttribute('crossorigin','*');
-  s0.parentNode.insertBefore(s1,s0);
-})();
 
 
 
@@ -52,6 +43,18 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function() {
   "use strict";
   $(document).on('turbolinks:load', function() {
+
+    window.$_Tawk = undefined;
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+      var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+      s1.async=true;
+      s1.src='https://embed.tawk.to/59cbca6ec28eca75e4622ae0/default';
+      s1.charset='UTF-8';
+      s1.setAttribute('crossorigin','*');
+      s0.parentNode.insertBefore(s1,s0);
+    })();
+
 
 
     var form = document.getElementById("needs-validation");
