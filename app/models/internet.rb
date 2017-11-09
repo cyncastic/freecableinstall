ecefa36114994e9c5bdf4c14379720680d9dfda7
John D. Moran <john@mtmcall.com>
2017-11-09 16:55:52 -0500

    model verification

--- app/models/internet.rb
+++ app/models/internet.rb
@@ -3,9 +3,16 @@ class Internet < ApplicationRecord
   belongs_to :provider
 
   validates :provider_id, presence: true
+
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
-  validates :phone, presence: true
+
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+
+  validates :phone, numericality: true, presence: true
+
+  validates :business_phone, numericality: true, presence: true
+
+  validates :zip, presence: true, numericality: true, length: { minimum: 5, maximum: 5 }
 
 end
--- app/models/voip.rb
+++ app/models/voip.rb
@@ -1,3 +1,18 @@
 class Voip < ApplicationRecord
+
   has_one :provider
+
+  # validates :provider_id, presence: true
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+
+  validates :phone, numericality: true, presence: true
+
+  validates :business_phone, numericality: true, presence: true
+
+  validates :zip, presence: true, numericality: true, length: { minimum: 5, maximum: 5 }
+
 end
