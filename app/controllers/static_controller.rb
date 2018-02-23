305f05663ab736511f3d0f4824a8392a31a7ca73
John D. Moran <john@mtmcall.com>
2018-02-23 10:29:25 -0500

    emails and a controller change

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,6 @@
 class StaticController < ApplicationController
 
-  skip_before_action :authorize, only: [:home, :resellers, :thankyou]
+  skip_before_action :authorize, only: [:home, :resellers, :thankyou, :tos]
 
   def home
   end
