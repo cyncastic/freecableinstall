a5e486ff54a90dae90df0a7d15f56636b82e5da5
John D. Moran <john@mtmcall.com>
2018-03-14 11:06:27 -0400

    zendesk chat instead of tawk

--- app/assets/javascripts/tawk.js
+++ app/assets/javascripts/tawk.js
@@ -2,19 +2,24 @@
   "use strict";
   $(document).on('turbolinks:load', function() {
 
-
     // Tawk snippet for adding the option to talk with customers on site
-    window.$_Tawk = undefined;
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-      var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-      s1.async=true;
-      s1.src='https://embed.tawk.to/59cbca6ec28eca75e4622ae0/default';
-      s1.charset='UTF-8';
-      s1.setAttribute('crossorigin','*');
-      s0.parentNode.insertBefore(s1,s0);
-    })();
+    // window.$_Tawk = undefined;
+    // var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    // (function(){
+    //   var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    //   s1.async=true;
+    //   s1.src='https://embed.tawk.to/59cbca6ec28eca75e4622ae0/default';
+    //   s1.charset='UTF-8';
+    //   s1.setAttribute('crossorigin','*');
+    //   s0.parentNode.insertBefore(s1,s0);
+    // })();
 
+    // Start of Zendesk Chat Script
+    window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+    $.src="https://v2.zopim.com/?5YTR2OIl44vigQIvCQzYt8XL0jJCDCE5";z.t=+new Date;$.
+    type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
 
   });
 }());
